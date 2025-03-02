7aab63d911a261aba49b0039c9f0a49f0701abc2
innocyber <innocentileka@gmail.com>
2025-03-02 14:20:21 +0100

    setup app typography

--- app/src/main/java/com/example/iweathercompose/utils/WeatherAppTypography.kt
+++ app/src/main/java/com/example/iweathercompose/utils/WeatherAppTypography.kt
@@ -1,2 +1,16 @@
 package com.example.iweathercompose.utils
 
+import android.R.attr.fontFamily
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.iweathercompose.ui.theme.Typography
+
+val appTypography = Typography(
+
+    displayLarge = Typography.bodyLarge.copy(fontFamily = appFontFamily, color = Color.White, fontSize = 100.sp),
+    displaySmall = Typography.bodyLarge.copy(fontFamily = appFontFamily, color = Color.White),
+    displayMedium = Typography.bodyLarge.copy(fontFamily = appFontFamily, color = Color.White),
+)
