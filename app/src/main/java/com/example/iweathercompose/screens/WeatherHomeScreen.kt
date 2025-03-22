a71bcf9f4f3df64866403e3e094de2dc3308e952
innocyber <innocentileka@gmail.com>
2025-03-22 19:01:50 +0100

    finished forecast weather data display on ui

--- app/src/main/java/com/example/iweathercompose/screens/WeatherHomeScreen.kt
+++ app/src/main/java/com/example/iweathercompose/screens/WeatherHomeScreen.kt
@@ -13,6 +13,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -37,6 +40,7 @@ import coil.request.ImageRequest
 import com.example.iweathercompose.R
 import com.example.iweathercompose.customUi.AppBackground
 import com.example.iweathercompose.data.dto.CurrentWeather
+import com.example.iweathercompose.data.dto.ForecastWeather
 import com.example.iweathercompose.utils.DEGREE
 import com.example.iweathercompose.utils.PERCENTAGE
 import com.example.iweathercompose.utils.formattedDate
@@ -72,9 +76,9 @@ fun WeatherHomeScreen(uiState: WeatherHomeUiState, modifier: Modifier = Modifier
                     .wrapContentSize()
             ) {
 
-                when(uiState){
-                    is WeatherHomeUiState.Error ->   Text("Error")
-                    is WeatherHomeUiState.Loading ->  Text("Loading....")
+                when (uiState) {
+                    is WeatherHomeUiState.Error -> Text("Error")
+                    is WeatherHomeUiState.Loading -> Text("Loading....")
                     is WeatherHomeUiState.Success -> WeatherSection(uiState.weather)
                 }
             }
@@ -86,21 +90,28 @@ fun WeatherHomeScreen(uiState: WeatherHomeUiState, modifier: Modifier = Modifier
 @Composable
 fun WeatherSection(weather: Weather, modifier: Modifier = Modifier) {
 
-    Column (modifier.padding(8.dp)){
-        CurrentWeatherSection(weather.currentWeather,modifier.weight(1f))
+    Column(modifier.padding(8.dp)) {
+        CurrentWeatherSection(weather.currentWeather, modifier.weight(1f))
+        ForeCastWeatherSection(weather.forecastWeather.list)
     }
-    
+
 }
 
 @Composable
 fun CurrentWeatherSection(currentWeather: CurrentWeather, modifier: Modifier = Modifier) {
-    Column (modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text("${currentWeather.name} ", style = MaterialTheme.typography.titleMedium)
-        Text(formattedDate(currentWeather.dt, pattern = "MMM dd yyyy"), style = MaterialTheme.typography.titleMedium)
+        Text(
+            formattedDate(currentWeather.dt, pattern = "MMM dd yyyy"),
+            style = MaterialTheme.typography.titleMedium
+        )
         Spacer(modifier = Modifier.height(20.dp))
         Text("${currentWeather.main?.temp}$DEGREE", style = MaterialTheme.typography.displayMedium)
         Spacer(modifier = Modifier.height(20.dp))
-        Text("Feels like ${currentWeather.main?.feelsLike}$DEGREE", style = MaterialTheme.typography.titleMedium)
+        Text(
+            "Feels like ${currentWeather.main?.feelsLike}$DEGREE",
+            style = MaterialTheme.typography.titleMedium
+        )
 
         Row(verticalAlignment = Alignment.CenterVertically) {
             AsyncImage(
@@ -113,30 +124,79 @@ fun CurrentWeatherSection(currentWeather: CurrentWeather, modifier: Modifier = M
                 modifier = Modifier.size(40.dp)
             )
 
-            Text("${currentWeather.weather?.get(0)?.description}", style = MaterialTheme.typography.titleMedium)
+            Text(
+                "${currentWeather.weather?.get(0)?.description}",
+                style = MaterialTheme.typography.titleMedium
+            )
         }
 
         Spacer(modifier = Modifier.height(20.dp))
-        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically,modifier = Modifier.fillMaxWidth() ) {
-            Column{
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column {
                 Text("Humidity ${currentWeather.main?.humidity}$PERCENTAGE")
                 Text("Pressure ${currentWeather.main?.pressure}hPa")
                 Text("Visibility ${currentWeather.visibility}km")
             }
             Spacer(modifier = Modifier.width(10.dp))
-            VerticalDivider(modifier = Modifier.height(100.dp).width(2.dp),thickness = 2.dp, color = Color.White)
+            VerticalDivider(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(2.dp), thickness = 2.dp, color = Color.White
+            )
 
             Spacer(modifier = Modifier.width(10.dp))
             Column {
-                Text("Sunrise ${formattedDate(currentWeather.sys?.sunrise,"hh:mm a")}")
-                Text("Sunset ${formattedDate(currentWeather.sys?.sunset,"hh:mm a")}")
+                Text("Sunrise ${formattedDate(currentWeather.sys?.sunrise, "hh:mm a")}")
+                Text("Sunset ${formattedDate(currentWeather.sys?.sunset, "hh:mm a")}")
             }
         }
     }
 }
 
+@Composable
+fun ForeCastWeatherSection(
+    forecastWeatherItems: List<ForecastWeather.ForecastItem?>?,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+        items(forecastWeatherItems!!.size) { index ->
+            ForecastItems(forecastWeatherItems[index]!!)
+        }
+    }
+}
+
+@Composable
+fun ForecastItems(forecastWeatherItem: ForecastWeather.ForecastItem, modifier: Modifier = Modifier) {
+
+    Card( colors = CardDefaults.cardColors(containerColor = Color.Black.copy(alpha = 0.5f))) {
+
+        Column(modifier = Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text( text = formattedDate(forecastWeatherItem.dt, "EEE"), style = MaterialTheme.typography.titleMedium)
+            Text(formattedDate(forecastWeatherItem.dt, pattern = "MMM dd"), style = MaterialTheme.typography.titleMedium)
+            Text(formattedDate(forecastWeatherItem.dt, "HH:mm"), style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(10.dp))
+            AsyncImage(model = ImageRequest.Builder(LocalContext.current)
+                .data(getIconUrl(forecastWeatherItem.weather?.get(0)?.icon.toString()))
+                .crossfade(true)
+                .build(),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp).padding(top = 4.dp, bottom = 4.dp)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text( text = "${forecastWeatherItem.main?.temp}$DEGREE", style = MaterialTheme.typography.titleMedium)
+
+        }
+    }
+
+}
+
 @Preview
 @Composable
 private fun WeatherHomeScreenPreview() {
-   // WeatherHomeScreen( )
+    // WeatherHomeScreen( )
 }
