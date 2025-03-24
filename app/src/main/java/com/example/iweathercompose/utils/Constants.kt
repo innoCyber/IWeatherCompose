be052f62d5cbbcb2420f150125424170e5222015
innocyber <innocentileka@gmail.com>
2025-03-24 06:22:37 +0100

    *update weather url

--- app/src/main/java/com/example/iweathercompose/utils/Constants.kt
+++ app/src/main/java/com/example/iweathercompose/utils/Constants.kt
@@ -5,8 +5,8 @@ const val DEGREE = "\u00b0 "
 const val PERCENTAGE = "\u0025"
 
 fun getCurrentWeatherUrl(latitude: Number, longitude: Number): String {
-    return "weather?lat=$latitude&lon=$longitude&appid=80036686eb91b4cb4e58e75d4b1da146&units=imperial"
+    return "weather?lat=$latitude&lon=$longitude&appid=$API_KEY&units=imperial"
 }
 fun getForecastWeatherUrl(latitude: Number, longitude: Number): String {
-    return "forecast?lat=$latitude&lon=$longitude&appid=80036686eb91b4cb4e58e75d4b1da146&units=imperial"
+    return "forecast?lat=$latitude&lon=$longitude&appid=$API_KEY&units=imperial"
 }
